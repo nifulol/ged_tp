b23c319fbf74d796fa5e181712b6672749f8302c
Julien Lavigne <Julien Lavigne@JulienLavigne>
2013-10-02 17:41:50 +0200

    creation controller

---
+++ ged_gphy/src/java/Controller/UtilisateurController.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+/**
+ *
+ * @author Julien Lavigne
+ */
+public class UtilisateurController {
+    
+}
