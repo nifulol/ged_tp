4bc1ba3ba55a9b493f5a6e2ccc3c0129e69f7b57
Julien Lavigne <Julien Lavigne@JulienLavigne>
2013-10-07 10:02:23 +0200

    Connexion à la base

--- ged_gphy/src/java/Controller/UtilisateurController.java
+++ ged_gphy/src/java/Controller/UtilisateurController.java
@@ -3,11 +3,37 @@
  * and open the template in the editor.
  */
 package Controller;
+import java.util.List;
+import model.Utilisateur;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import util.*;
 
 /**
  *
  * @author Julien Lavigne
  */
 public class UtilisateurController {
+    
+    private Utilisateur user;
+   
+    public UtilisateurController(Utilisateur user)
+    {
+        this.user=user;
+    }
+   public void connection(){
+       Session session = HibernateUtil.getSessionFactory().openSession();
+       session.beginTransaction();
+      
+       Query sqlQuery = session.createSQLQuery("Select * from utilisateur where login="+user.getLogin()+"password"+user.getPassword());
+       List<Utilisateur> List_user = sqlQuery.list();
+       user=List_user.get(0);
+       
+       
+   }
+   
+    
+    
     
 }
